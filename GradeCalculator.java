9dcdd7afd7b9ddfa8b2f7736278ac9a6f00c8e4b
Sivasubramani <sivasubramani099@gmail.com>
2018-06-23 00:55:29 +0530

    Add files via upload
    added grade calculator,
    need some updates

---
+++ GradeCalculator.java
@@ -0,0 +1,74 @@
+package checkingTemporary;
+
+import java.util.Scanner;
+/*
+ * @Sivasubramani_D
+ * */
+public class GradeCalculator {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the No. of Subjects:");
+		int noOfSubjects = sc.nextInt();
+		int arrearCount = 0;
+		int credit[] = new int[noOfSubjects];
+		int gradePoint[] = new int[noOfSubjects];
+		int result[] = new int [noOfSubjects];
+		double resultSum=0;
+		double creditSum=0;
+		System.out.println("Enter the credits and grade point for each subject (Seperated by space):");
+		System.out.println("Credits:\tGrade points:\n");
+		for(int i=0; i<noOfSubjects; i++) {
+			credit[i]=sc.nextInt();
+			char ch = sc.next().charAt(0);
+			switch(ch) {
+			case 'S':
+			case 's' : gradePoint[i] = 10; break;
+			case 'A':
+			case 'a' : gradePoint[i] = 9; break;
+			case 'B':
+			case 'b' : gradePoint[i] = 8; break;
+			case 'C':
+			case 'c' : gradePoint[i] = 7; break;
+			case 'D':
+			case 'd' : gradePoint[i] = 6; break;
+			case 'E': 
+			case 'e' : gradePoint[i] = 5; break;
+			case 'U':
+			case 'u' : gradePoint[i] = 4; break;
+			}
+			result[i] = credit[i]*gradePoint[i];
+			resultSum += result[i];
+			creditSum += credit[i];
+			if(gradePoint[i]<5) {
+				arrearCount++;
+			} 
+		}
+		//SGPA
+		double sgpa = resultSum/creditSum;
+		System.out.println("For verification,..\n");
+		System.out.println("Subject No--->\t"+"Credits * Grade point\n");
+		for(int i=0; i<noOfSubjects; i++) {
+			System.out.println("Subject "+(i+1)+" --->\t"+credit[i]+" * "+gradePoint[i]+" = "+(credit[i]*gradePoint[i]));
+			}
+		if(arrearCount>0) {
+			System.out.println("No. of arrears: "+arrearCount);
+		}
+		System.out.println("\nSGPA:\t"+sgpa);
+		//CGPA
+		char ch = ' ';
+		double cgpaSum = 0;
+		double cgpa = 0;
+		int previousCredits = 136; //Credits upto 5th Semester
+		System.out.println("Do you want to calculate your CGPA: (Y/N) ");
+		ch=sc.next().charAt(0);
+		if(ch==('y'|'Y')) {
+			System.out.println("Enter your 5th semester CGPA: ");
+			float lastCGPA = sc.nextFloat();
+			cgpaSum = (lastCGPA*previousCredits)+(sgpa*creditSum);
+			cgpa = cgpaSum/(previousCredits+creditSum);
+			System.out.println("Your SGPA is\t"+sgpa);
+			System.out.println("Your CGPA is\t"+cgpa);
+		}
+		sc.close();		
+	}
+}
